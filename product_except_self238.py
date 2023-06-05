319c412359b415bce5e087e7cf2947daa9a64f50
Adelaja Qowiyyu <adexolamilekan46@gmail.com>
2023-06-05 01:42:00 +0100

    optimized solution with lesser memory usage

--- product_except_self238.py
+++ product_except_self238.py
@@ -1,31 +1,24 @@
 """
-The approach is to create two lists one which holds the product of numbers before every number in the list
-another which holds the product of numbers after every number in the list
-then multiply the corresponding pre-product and post-product to get the product of the array except the value 
-of each index
-
-Approach Credit: Neetcode.io Solution
+This approach is an optimized version of the first acceptance. It uses only one list to store the products.
 """
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         product = 1
-        prefix = [1]
-        postfix = [1]
+        result = []
 
         #filling the prefix list
-        for num in nums:
-            product *= num
-            prefix.append(product)
+        result.append(product) #making the prefix of the first number 1 by default
+
+        for index in range(0, len(nums)-1, 1):
+            product *= nums[index]
+            result.append(product)
+        print(result)
         
         #filling the postfix list
         product = 1
-        for index in range(len(nums)-1, -1, -1):
+        for index in range(len(nums)-1, 0, -1):
             product *= nums[index]
-            postfix.insert(0, product)
-        
-        result = []
-        #generate the product of array except self list
-        for i in range(len(prefix)-1):
-            result.append(prefix[i]*postfix[i+1])
-        
+            result[index-1] *= product 
+
         return result
+        
